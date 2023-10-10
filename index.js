d23f05e52785aa0a532827e1400a8a13bd38aa07
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-10-10 19:13:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 // Middlewaret
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('data', function (req, res) { if (req.method == "POST") {return JSON.stringify(req.body)} })
 
