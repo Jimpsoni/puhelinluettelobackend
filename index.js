44c3894761a3b4e3b8e659c2c2a33158c23332b7
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-10-11 18:20:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// Connect to DB
+const Person = require('./models/person')
+
+
 // määritellään express
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
+
+require('dotenv').config()
+
+app.use(express.static('dist'))
+
+const morgan = require('morgan')
 const cors = require('cors')
 
 // Middlewaret
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 
 morgan.token('data', function (req, res) { if (req.method == "POST") {return JSON.stringify(req.body)} })
 
@@ -25,43 +33,12 @@ app.use(
 
 )
 
-
-// Määritellään yhteystiedot
-let persons = [
-      {
-        "name": "Arto Hellas",
-        "number": "321",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      },
-      {
-        "name": "Peppi",
-        "number": "123",
-        "id": 5
-      }
-]
-
-
-// funktio joka luo uuden ID:n
-const generateID = () => Math.floor(Math.random() * 1000)
-
-
 // Määritellään API kutsut
-app.get('/api/persons', (req, res) => { res.json(persons) })
+app.get('/api/persons', (req, res) => { 
+  Person.find({}).then(person => {
+  res.json(person) })
+})
+
 
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
@@ -84,27 +61,29 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
+
+
 app.post("/api/persons", (req, res) => {
     const body = req.body
-    const nameExists = persons.find(person => person.name == body.name)
     
-    if (nameExists) return res.json({"error": "Person with that name already exists"})
     if (!body.name || !body.number) return res.json({"error": "missing data, check that you have name and number"})
 
-    const newPerson = {
+    const newPerson = new Person({
         "name": body.name,
         "number": body.number,
-        "id": generateID()
-    }
+    })
 
     // Lisätään uusi henkilö yhteystietoihin
-    persons = persons.concat(newPerson)
-    res.status(200).end()
+    newPerson.save().then(person => {
+      res.json(person)
+    })
 })
 
 
+
+
 // Käynnistetään serveri
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
