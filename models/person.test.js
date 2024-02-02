64e26fc0bae4b14848fb754811f7e864e56381af
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2024-02-02 13:45:57 +0200

    Create person.test.js

---
+++ models/person.test.js
@@ -0,0 +1,12 @@
+// These are mock tests for CICD pipeline
+test('Mock tests exists', () => {
+    expect(1).toBe(1)
+})
+
+test('1 + 1 does equal 2', () => {
+    expect(1 + 1).toBe(2)
+})
+
+test('Hello world is the common phrase for developers', () => {
+    expect("hello world").toBe("hello world")
+})
